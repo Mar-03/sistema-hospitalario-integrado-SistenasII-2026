495f7693d19b78bf41ab519edceb744d7d9c9f2e
Mar-03 <mlopezf16@miumg.edu.gt>
2026-09-14 22:41:49 -0600

    chore(asii-15): align sqlite db path to database/prescripciones.sqlite

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 return [
     'driver' => getenv('MOD15_DB_DRIVER') ?: 'sqlite',
-    'path' => getenv('MOD15_DB_PATH') ?: __DIR__ . '/../storage/mod15.sqlite',
+    'path' => getenv('MOD15_DB_PATH') ?: __DIR__ . '/../database/prescripciones.sqlite',
     'host' => getenv('MOD15_DB_HOST') ?: '127.0.0.1',
     'port' => getenv('MOD15_DB_PORT') ?: '5432',
     'database' => getenv('MOD15_DB_NAME') ?: 'mod15',
